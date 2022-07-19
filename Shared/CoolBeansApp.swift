a853fc8953227ea5e00c00e294c95c7e7cbc0aab
Tarik <tarikmerrylees@gmail.com>
2022-07-19 08:59:47 +0100

    Initial Commit

---
+++ Shared/CoolBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolBeansApp.swift
+//  Shared
+//
+//  Created by Tarik Merrylees on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoolBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
