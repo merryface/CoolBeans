20b13ea08fbd3d4eb92c0a979484b99dab938b70
Tarik <tarikmerrylees@gmail.com>
2022-07-31 19:43:45 +0100

    create history and methods to save

--- Shared/Serving.swift
+++ Shared/Serving.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct Serving: Identifiable, Codable {
+    var id: UUID
+    let name: String
+    let description: String
+    let caffeine: Int
+    let calories: Int
+}
